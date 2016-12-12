26fcaba638cda7d6af830c6b5867da9cbad7c0e1
msosborne3 <msosborne@ymail.com>
2016-12-11 21:10:32 -0600

    add post '/signup' route to get user signup info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,16 @@ class UsersController < ApplicationController
       erb :"users/create_user"
     end
   end
+
+  # Gets the input from the form and creates a new user
+  post '/signup' do
+    # If the user did not enter a username and password then they have to fill out the form again
+    if params[:username] == "" || params[:password] == ""
+      redirect to '/signup'
+    else  # Otherwise a new user is created with the info and the user is then redirected to tweets
+      @user = User.create(username: params[:username], password: params[:password])
+      session[:user_id] = @user.id
+      redirect to "/users/#{current_user.slug}"
+    end
+  end
 end
