82539e18992add18612999ab0c821769bc447d62
msosborne3 <msosborne@ymail.com>
2016-12-11 20:47:16 -0600

    add methods to slug a username and to find_by_slug

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ActiveRecord::Base
   has_many :items
 
   has_secure_password
+
+  # Converts a username from "test 123" to "test-123"
+  def slug
+    username.downcase.gsub(' ', '-')
+  end
+
+  # Finds the user by the slugified version of its username
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug}
+  end
 end
