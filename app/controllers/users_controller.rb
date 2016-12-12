2750af64883d0f840ee6859b0bb90c3827e6660c
msosborne3 <msosborne@ymail.com>
2016-12-11 21:14:50 -0600

    add logout route to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,4 +48,14 @@ class UsersController < ApplicationController
       redirect to '/login'
     end
   end
+
+  # If a user is already logged in then they are able to logout
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
 end
